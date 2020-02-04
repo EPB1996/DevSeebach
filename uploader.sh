c0b766c4215055bbd2af454995b795b03e557e23
Etienne Baumgartner <baume96@gmail.com>
2020-02-04 17:20:26 +0100

    file Transfer setup

--- uploader.sh
+++ uploader.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-sftp $1:/C:/Users/EPB/Pictures/FotoWall/$2 <<EOF
+sftp -oPort=2410 $1:/C:/Users/EPB/Pictures/FotoWall/$2 <<EOF
 lcd /home/epb1996/Pictures/FotoWall/$2
-put *.jpg
+mput *.jpg
 exit
 EOF
+rm -rf /home/epb1996/Pictures/FotoWall/$2/*.jpg
